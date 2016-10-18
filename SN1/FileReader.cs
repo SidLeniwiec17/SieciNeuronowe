4fe83f8e37965a35675423a35ad53277bdca8f00
SidLeniwiec <piotrmichalik17@wp.pl>
2016-10-18 23:09:46 +0200

    robie wyniki z testowego

--- SN1/FileReader.cs
+++ SN1/FileReader.cs
@@ -20,10 +20,10 @@ namespace SN1
 
         public object DialogResult { get; private set; }
 
-        public List<ThreeVariableItem> GetItems()
+       public List<ThreeVariableItem> GetItems()
         {
             OpenFileDialog choofdlog = new OpenFileDialog();
-            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.Filter = "All Files (.)|*.*";
             choofdlog.FilterIndex = 1;
             choofdlog.Multiselect = false;
             choofdlog.ShowDialog();
@@ -31,33 +31,13 @@ namespace SN1
 
             using (TextReader reader = File.OpenText(fileName))
             {
-                var list = new List<ThreeVariableItem>();
-                var stringList = new List<string>();
                 var csv = new CsvReader(reader);
+                csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
 
-                csv.Configuration.CultureInfo = CultureInfo.CurrentCulture;
-
-                try {
-                    while (csv.Read())
-                    {
-
-                        var field1 = Double.Parse(csv.GetField<string>(0));
-                        var field2 = Double.Parse(csv.GetField<string>(1));
-                        var field3 = csv.GetField<int>(2);
-                        list.Add(new ThreeVariableItem() { x = field1, y = field2, cls = field3 });
-                    }
-                }
-                catch
-                {
-                 while(csv.Read())
-                    {
-                        stringList.Add(csv.GetField<string>(0));
-                    }   
-                }
-                return list;
-                //                var records = csv.GetRecords<ThreeVariableItem>().ToList();
-                //return records;
+                var records = csv.GetRecords<ThreeVariableItem>().ToList();
+                return records;
             }            
+            
         }
     }
 }
